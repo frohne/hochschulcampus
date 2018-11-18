f9ece0f098e7711fb3337b8904184cbe9d3baa7c
Lennart Kraft <lennartkraft@gmx.de>
2018-11-18 11:40:36 +0100

    Steuerung(Auswahl, Translation, Rotation, Skalierung, Reset) für Android

---
+++ Assets/HSHL2030 Scripts/ObjectRotation.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*
+ * Script wich handles the rotation sliders and rotates the selected object
+ * 
+ * Lennart
+ */
+
+public class ObjectRotation : MonoBehaviour
+{
+
+    private GameObject selectedObject = null;
+    public Slider sliderX = null;
+    public Slider sliderY = null;
+    public Slider sliderZ = null;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(selectedObject != null)
+        {
+            selectedObject.transform.rotation = Quaternion.Euler(sliderX.value, sliderY.value, sliderZ.value);
+
+        }
+    }
+
+    public void setSelectedObject(GameObject obj)
+    {
+        selectedObject = obj;
+        
+        sliderX.value = selectedObject.transform.rotation.eulerAngles.x;
+
+        sliderY.value = selectedObject.transform.rotation.eulerAngles.y;
+
+        sliderZ.value = selectedObject.transform.rotation.eulerAngles.z;
+    }
+
+    public void resetRotation()
+    {
+        selectedObject.transform.rotation = Quaternion.Euler(selectedObject.GetComponent<RootSettings>().getRootScaling());
+        setSelectedObject(selectedObject);
+    }
+}
---
+++ Assets/HSHL2030 Scripts/ObjectTranslation.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*
+ * Script wich handles the translation sliders and translates the selected object
+ * 
+ * Lennart
+ */
+public class ObjectTranslation : MonoBehaviour
+{
+
+    private GameObject selectedObject = null;
+    public Slider sliderX = null;
+    public Slider sliderY = null;
+    public Slider sliderZ = null;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(selectedObject != null)
+        {
+            selectedObject.transform.position = new Vector3(sliderX.value, sliderY.value, sliderZ.value);
+            
+        }
+	}
+
+    public void setSelectedObject(GameObject obj)
+    {
+        //0.5 => Wertebereich
+        selectedObject = obj;
+        sliderX.value = selectedObject.transform.position.x;
+        sliderX.minValue = selectedObject.transform.position.x - 0.5f;
+        sliderX.maxValue = selectedObject.transform.position.x + 0.5f;
+
+        sliderY.value = selectedObject.transform.position.y;
+        sliderY.minValue = selectedObject.transform.position.y - 0.5f;
+        sliderY.maxValue = selectedObject.transform.position.y + 0.5f;
+
+        sliderZ.value = selectedObject.transform.position.z;
+        sliderZ.minValue = selectedObject.transform.position.z - 0.5f;
+        sliderZ.maxValue = selectedObject.transform.position.z + 0.5f;
+    }
+
+    public void resetPosition()
+    {
+        selectedObject.transform.position = selectedObject.GetComponent<RootSettings>().getRootPosition();
+        setSelectedObject(selectedObject);
+    }
+}
