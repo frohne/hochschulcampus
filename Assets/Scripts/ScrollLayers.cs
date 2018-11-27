ba6a4d96c9cb63fa584152e361859b1f577f9b2a
Karen <karen.boerner@raumtaenzer.com>
2018-11-27 23:32:29 +0100

    Layer List

---
+++ Assets/Scripts/ScrollLayers.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollLayers : MonoBehaviour {
+
+    public ScrollRect scrollView;
+    public GameObject scrollContent;
+    public GameObject scrollItemLayer;
+    public GameObject layerObject;
+     
+    string[] layernames;
+    public TextMeshProUGUI objectName;
+    int count;
+	// Use this for initialization
+	void Start () {
+        //layernames = layerObject.GetComponentsInChildren<GameObject>().name;
+        objectName.text = layerObject.name;
+        count = layerObject.GetComponent<Transform>().childCount;
+        generateLayerItem();
+      
+        print(layerObject.GetComponent<Transform>().GetChild(0).name);
+        print(layerObject.GetComponent<Transform>().GetChild(1).name);
+        
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void generateLayerItem()
+    {
+        GameObject scrollItemLayerObj = Instantiate(scrollItemLayer);
+        scrollItemLayerObj.transform.SetParent(scrollContent.transform, false);
+        scrollItemLayerObj.GetComponentInChildren<TextMeshProUGUI>().text = layerObject.GetComponent<Transform>().GetChild(0).name;
+    }
+
+   
+}
